ee57051be96df86184f3c1600b96fd7289176630
Ubuntu <megamegajunk@gmail.com>
2020-02-05 16:21:18 +0000

    tested2

--- demofordevops1/src/main/java/demofordevops1/test2.java
+++
@@ -1,9 +0,0 @@
-package demofordevops1;
-
-public class test2 {
-
-	public static void main(String[] args) {
-		System.out.println("112233");
-	}
-
-}
--- demofordevops1/src/main/java/demofordevops1/test3.java
+++
@@ -1,9 +0,0 @@
-package demofordevops1;
-
-public class test3 {
-
-	public static void main(String[] args) {
-		System.out.println("112233");
-	}
-
-}
